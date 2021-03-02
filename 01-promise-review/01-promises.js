50fcc137157facec86b1878cebd852bf3dcb1a15
Elizabeth Harris <elizabeth_harris@homedepot.com>
2021-03-02 10:20:31 -0600

    Adds links to lessons

--- 01-promise-review/01-promises.js
+++ 01-promise-review/01-promises.js
@@ -1,18 +1,13 @@
-function myDisplayer(some) {
-  console.log(some);
-}
-
 let isZero = function(x){
   return new Promise(function(myResolve, myReject) {
     if (x == 0 && typeof(x) === Number) {
-      // if the value is 0 and a number, send a resolve with the message of "OK"
-      myResolve("OK");
+      // if the value is 0 and a number, resolve with the message of "OK"
     } else {
-      myReject("Error");
+      // if the value is not 0 or a number, reject with the message of "Error"
     }
   });
 }
 
 isZero(0)
-    .then((value) => myDisplayer(value))
-    .catch((error) => myDisplayer(error))
+    .then((value) => console.log(value))
+    .catch((error) => console.log(error))
